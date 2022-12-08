fb194f0a2fd1f938b93d2884a5655373d3bb2d33
Mustafacifc <114651181+Mustafacifc@users.noreply.github.com>
2022-12-08 09:32:10 +0300

    Proje dosyası ekle.

---
+++ metot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ToplamaSinifi toplamaSinifi = new ToplamaSinifi();
+            Console.WriteLine(toplamaSinifi.Topla(10.5,1));
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ metot/Toplamaislemi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metot
+{
+    class ToplamaSinifi 
+    {
+        public int Topla(int sayi1, int sayi2)
+        {
+            Console.WriteLine("Tam sayı metotu çalıştı");
+            return sayi1 + sayi2;
+        }
+        public double Topla(double sayi1, double sayi2)
+        {
+            Console.WriteLine("Ondalıklı sayı metotu çalıştı");
+            return sayi1 + sayi2;
+        }
+        public string Topla(string metin1 , string metin2)
+        {
+            Console.WriteLine("String metotu çalıştı");
+            return metin1 + metin2;
+        }
+    }
+}
